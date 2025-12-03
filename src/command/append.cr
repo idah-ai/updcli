9ba2973e001ddcbc4d62c9b52cd07334a134fcf1
Brian West <brian.west@ingedata.ai>
2025-12-03 13:37:35 +0700

    append command within transaction

--- src/command/append.cr
+++ src/command/append.cr
@@ -22,89 +22,94 @@ module Command
       input = option("input") || "/dev/stdin"
 
       append_commands = [
-        [
+        {
           "init",
-          ->(args: Array(Argument)) {Command::Init.new(args, root).run}
-        ],
-        [
+          ->(args: Array(Argument)) {Command::Init.new(args, root, "init").run}
+        },
+        {
           "dataset:list",
-          ->(args: Array(Argument)) {Command::Dataset::List.new(args, root).run}
-        ],
-        [
+          ->(args: Array(Argument)) {Command::Dataset::List.new(args, root, "dataset list").run}
+        },
+        {
           "dataset:create",
-          ->(args: Array(Argument)) {Command::Dataset::Create.new(args, root).run}
-        ],
-        [
+          ->(args: Array(Argument)) {Command::Dataset::Create.new(args, root, "dataset create").run}
+        },
+        {
           "dataset:delete",
-          ->(args: Array(Argument)) {Command::Dataset::Delete.new(args, root).run}
-        ],
-        # [
+          ->(args: Array(Argument)) {Command::Dataset::Delete.new(args, root, "dataset delete").run}
+        },
+        # {
         #   "dataset:update",
-        #   ->(args: Array(Argument)) {Command::Dataset::Update.new(args, root).run}
-        # ],
-        [
+        #   ->(args: Array(Argument)) {Command::Dataset::Update.new(args, root, "dataset update").run}
+        # },
+        {
           "entry:create",
-          ->(args: Array(Argument)) {Command::Entry::Create.new(args, root).run}
-        ],
-        [
+          ->(args: Array(Argument)) {Command::Entry::Create.new(args, root, "entry create").run}
+        },
+        {
           "entry:delete",
-          ->(args: Array(Argument)) {Command::Entry::Delete.new(args, root).run}
-        ],
-        # [
+          ->(args: Array(Argument)) {Command::Entry::Delete.new(args, root, "entry delete").run}
+        },
+        # {
         #   "entry:update",
-        #   ->(args: Array(Argument)) {Command::Entry::Update.new(args, root).run}
-        # ],
-        [
+        #   ->(args: Array(Argument)) {Command::Entry::Update.new(args, root, "entry update").run}
+        # },
+        {
           "media:create",
-          ->(args: Array(Argument)) {Command::Media::Create.new(args, root).run}
-        ],
-        [
+          ->(args: Array(Argument)) {Command::Media::Create.new(args, root, "media create").run}
+        },
+        {
           "media:delete",
-          ->(args: Array(Argument)) {Command::Media::Delete.new(args, root).run}
-        ],
-        # [
+          ->(args: Array(Argument)) {Command::Media::Delete.new(args, root, "media delete").run}
+        },
+        # {
         #   "media:update",
-        #   ->(args: Array(Argument)) {Command::Media::Update.new(args, root).run}
-        # ],
-        [
+        #   ->(args: Array(Argument)) {Command::Media::Update.new(args, root, "media update").run}
+        # },
+        {
           "annotation:create",
-          ->(args: Array(Argument)) {Command::Annotation::Create.new(args, root).run}
-        ],
-        [
+          ->(args: Array(Argument)) {Command::Annotation::Create.new(args, root, "annotation create").run}
+        },
+        {
           "annotation:delete",
-          ->(args: Array(Argument)) {Command::Annotation::Delete.new(args, root).run}
-        ],
-        # [
+          ->(args: Array(Argument)) {Command::Annotation::Delete.new(args, root, "annotation delete").run}
+        },
+        # {
         #   "annotation:update",
-        #   ->(args: Array(Argument)) {Command::Annotation::Update.new(args, root).run}
-        # ],
-      ]
+        #   ->(args: Array(Argument)) {Command::Annotation::Update.new(args, root, "annotation update").run}
+        # },
+      ].as(Array(Tuple(String, Proc(Array(Command::Argument), (DB::ExecResult | Nil)))))
 
       File.open(input, "r") do |file|
         i = 1
-        file.each_line do |line|
-          # Here you would parse the JSON line and append it to the UPD.
-          # This is a placeholder for the actual append logic.
+        begin
+          root.database.exec("BEGIN TRANSACTION")
+          file.each_line do |line|
+            # Here you would parse the JSON line and append it to the UPD.
+            # This is a placeholder for the actual append logic.
 
-          # TODO: Use of JSON Pull Parser for efficiency.
+            # TODO: Use of JSON Pull Parser for efficiency.
 
-          appendInstruction = AppendInstruction.from_json(line.strip)
-          puts appendInstruction
+            appendInstruction = AppendInstruction.from_json(line.strip)
 
-          command = append_commands.find(&.[0].==(appendInstruction.command))
+            command = append_commands.find(&.[0].==(appendInstruction.command))
 
-          if !command
-            raise "command not found: #{appendInstruction.command} (line #{i})"
-          else
-            command[1]
-            .as(Proc(Array(Argument), Nil)) #...
-            .call(
-              appendInstruction.args.map do |k, v| # mapping from_json ?
-                Argument.new(k, :optlong, v.to_s)
-              end
-            )
+            if !command
+              raise "command not found: #{appendInstruction.command} (line #{i})"
+            else
+              command[1]
+              .call(
+                appendInstruction.args.map do |k, v| # mapping from_json ?
+                  Argument.new(k, :optlong, v.to_s)
+                end
+              )
+            end
+            i+=1
           end
-          i+=1
+          root.database.exec("COMMIT")
+        rescue e
+          puts e
+          root.database.exec("ROLLBACK")
         end
       end
     end

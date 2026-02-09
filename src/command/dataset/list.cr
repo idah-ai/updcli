520639097c13542fae7f48dc582408edfe1c4130
Brian West <brian.west@ingedata.ai>
2026-02-09 16:46:46 +0700

    remove remnants

--- src/command/dataset/list.cr
+++ src/command/dataset/list.cr
@@ -5,25 +5,17 @@ module Command
 
       def run_impl
         datasets = root.database.query_all("SELECT id, name, modality, metadata FROM datasets") do |dataset|
-          h = {
+          {
             id: dataset.read(String),
             name: dataset.read(String),
             modality: dataset.read(String),
             metadata: dataset.read(String)
           }
-          # puts(h)
-          # {
-          #   id: h[:id],
-          #   name: h[:name],
-          #   modality: h[:modality],
-          #   metadata: JSON.parse(h[:metadata])
-          # }
         end
         if datasets.empty?
           puts "No datasets found."
         else
           datasets.each do |dataset|
-            # puts JSON.parse(dataset[:metadata])
             puts dataset.to_json
           end
         end
